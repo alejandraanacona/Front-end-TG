becce2df664116ef51225cb7b85da47a5df9d699
Alejandra Anacona <alejandra.anacona@correounivalle.edu.co>
2023-04-27 18:44:32 -0500

    login funcional

---
+++ src/components/ConfParameters.vue
@@ -0,0 +1,13 @@
+<template>
+    
+
+</template>
+  
+  <script>
+  export default {
+    name: 'ConfParameters',
+    props: {
+      msg: String
+    }
+  }
+  </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+//import HomeView from '../views/HomeView.vue'
+
+
 
 const routes = [
+  
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'LoginDeep',
+    component: () => import('../components/LoginDeep.vue')
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/forgot-password',
+    name: 'forgot-password',
+    component: () => import('../components/ForgotPassword.vue')
+  },
+
+  {
+    path: '/config',
+    name: 'ConfParameters',
+    component: () => import('../components/ConfParameters.vue')
+  },
+  
+  
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+
 
 export default router
